5fa60c4bca7252aaf9794118a5bfbae86e843d3d
dominikn <dominik.nwk@gmail.com>
2024-07-10 22:46:45 +0200

    fixed launcher script

--- snap/local/launcher.sh
+++ snap/local/launcher.sh
@@ -1,38 +1,26 @@
 #!/bin/bash -e
 
-log() {
-    local message="$1"
-    # Log the message with logger
-    logger -t "${SNAP_NAME}" "launcher: $message"
-}
-
-log_and_echo() {
-    local message="$1"
-    # Log the message with logger
-    logger -t "${SNAP_NAME}" "configure hook: $message"
-    # Echo the message to standard error
-    echo >&2 "$message"
-}
+source $SNAP/usr/bin/utils.sh
 
 # Iterate over the snap parameters and retrieve their value.
 # If a value is set, it is forwarded to the launch file.
 
-OPTIONS="\
-    name \
-    parent-frame \
-    camera-model \
-    cam-pos-x \
-    cam-pos-y \
-    cam-pos-z \
-    cam-roll \
-    cam-pitch \
-    cam-yaw \
-    params-file \
-"
+OPTIONS=(
+  name
+  parent-frame
+  camera-model
+  cam-pos-x
+  cam-pos-y
+  cam-pos-z
+  cam-roll
+  cam-pitch
+  cam-yaw
+  params-file
+)
 
 LAUNCH_OPTIONS=""
 
-for OPTION in ${OPTIONS}; do
+for OPTION in "${OPTIONS[@]}"; do
   VALUE="$(snapctl get driver.${OPTION})"
   if [ -n "${VALUE}" ]; then
     OPTION_WITH_UNDERSCORE=$(echo ${OPTION} | tr - _)
