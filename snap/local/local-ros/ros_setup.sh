5e90e8087389bb61511e694ce73ca94c7262e56c
dominikn <dominik.nwk@gmail.com>
2024-07-11 12:18:37 +0200

    copy ros.env if for some reason don't exist

--- snap/local/local-ros/ros_setup.sh
+++ snap/local/local-ros/ros_setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -e
 
-source $SNAP_COMMON/ros.env
+source $SNAP/usr/bin/utils.sh
 
-exec $@
+# Path to the ros.env file
+ROS_ENV_FILE="$SNAP_COMMON/ros.env"
+
+# Check if the ros.env file exists
+if [[ ! -f "$ROS_ENV_FILE" ]]; then
+    log "Copying ros.env file to $SNAP_COMMON"
+    cp -r ${SNAP}/usr/share/${SNAP_NAME}/config/ros.env ${SNAP_COMMON}/
+fi
+
+# Source the ros.env file
+source "$ROS_ENV_FILE"
+
+# Execute the passed command
+exec "$@"
